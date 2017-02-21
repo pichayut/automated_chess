284cbf23873371f3cb03dee4917b070cd184e785
Pichayut Liamthong <pichl@uw.edu>
2017-02-21 15:30:35 -0800

    Fixed divideCutoff = 4

--- src/chess/bots/ParallelSearcher.java
+++ src/chess/bots/ParallelSearcher.java
@@ -14,9 +14,8 @@ public class ParallelSearcher<M extends Move<M>, B extends Board<M, B>> extends
         AbstractSearcher<M, B> {
 	
 	private static ForkJoinPool POOL = new ForkJoinPool();
-	private static final int DIVIDE_CUTOFF = 32;
+	private static final int DIVIDE_CUTOFF = 4;
 	
-	@SuppressWarnings("serial")
 	static class GetBestMoveTask<M extends Move<M>, B extends Board<M, B>> extends RecursiveTask<BestMove<M>> {
 		int divideCutoff;
 		int cutoff;
@@ -25,21 +24,14 @@ public class ParallelSearcher<M extends Move<M>, B extends Board<M, B>> extends
 		List<M> moves;
 		B board;
 		Evaluator<B> e;
-		boolean hasToSwitch;
 		
-		public GetBestMoveTask(List<M> moves, B board, int l, int r, Evaluator<B> e, int depth, int cutoff, int divideCutoff, boolean  hasToSwitch) {
-			if(moves == null) {
-				this.moves = board.generateMoves();
-				this.r = this.moves.size();
-			} else {
-				this.moves = moves;
-				this.r = r;
-			}
+		public GetBestMoveTask(List<M> moves, B board, int l, int r, Evaluator<B> e, int depth, int cutoff, int divideCutoff) {
+			this.moves = moves;
+			this.r = r;
 			this.board = board;
 			this.cutoff = cutoff;
 			this.divideCutoff = divideCutoff;
 			this.depth = depth;
-			this.hasToSwitch = hasToSwitch;
 			this.l = l;
 			this.e = e;
 		}
@@ -50,6 +42,10 @@ public class ParallelSearcher<M extends Move<M>, B extends Board<M, B>> extends
 		
 		protected BestMove<M> compute() {
 			if(this.size() <= this.divideCutoff) {
+				if(moves == null) {
+					this.moves = board.generateMoves();
+					this.r = this.moves.size();
+				} 
 				this.board = this.board.copy();
 				if(depth <= cutoff || this.size() == 0) {
 		    		return SimpleSearcher.minimax(e, board, depth, moves);
@@ -57,12 +53,12 @@ public class ParallelSearcher<M extends Move<M>, B extends Board<M, B>> extends
 				List<GetBestMoveTask<M, B>> taskList = new ArrayList<GetBestMoveTask<M, B>>();
 				for(int i = l; i < r - 1; i++) {
 					board.applyMove(moves.get(i));
-					taskList.add(new GetBestMoveTask<M, B>(null, board.copy(), 0, -1, e, depth - 1, cutoff, divideCutoff, true));
+					taskList.add(new GetBestMoveTask<M, B>(null, board.copy(), 0, -1, e, depth - 1, cutoff, divideCutoff));
 					taskList.get(i - l).fork();
 					board.undoMove();
 				}
 				board.applyMove(moves.get(r - 1));
-				taskList.add(new GetBestMoveTask<M, B>(null, board, 0, -1, e, depth - 1, cutoff, divideCutoff, true));
+				taskList.add(new GetBestMoveTask<M, B>(null, board, 0, -1, e, depth - 1, cutoff, divideCutoff));
 				int bestValue = taskList.get(this.size() - 1).compute().negate().value;
 				M bestMove = moves.get(r - 1);
 				for(int i = 0; i < taskList.size() - 1; i++) {
@@ -75,8 +71,8 @@ public class ParallelSearcher<M extends Move<M>, B extends Board<M, B>> extends
 		    	return new BestMove<M>(bestMove, bestValue);
 			}
 			
-			GetBestMoveTask<M, B> leftTask = new GetBestMoveTask<M, B>(moves, board, l, l + (r - l) / 2, e, depth, cutoff, divideCutoff, false);
-			GetBestMoveTask<M, B> rightTask = new GetBestMoveTask<M, B>(moves, board, l + (r - l) / 2, r, e, depth, cutoff, divideCutoff, false);
+			GetBestMoveTask<M, B> leftTask = new GetBestMoveTask<M, B>(moves, board, l, l + (r - l) / 2, e, depth, cutoff, divideCutoff);
+			GetBestMoveTask<M, B> rightTask = new GetBestMoveTask<M, B>(moves, board, l + (r - l) / 2, r, e, depth, cutoff, divideCutoff);
 			
 			leftTask.fork();
 			BestMove<M> answer = rightTask.compute();
@@ -91,7 +87,7 @@ public class ParallelSearcher<M extends Move<M>, B extends Board<M, B>> extends
 	
     public M getBestMove(B board, int myTime, int opTime) {
     	List<M> moves = board.generateMoves();
-    	BestMove<M> bestMove = POOL.invoke(new GetBestMoveTask<M, B>(moves, board, 0, moves.size(), this.evaluator, ply, cutoff, DIVIDE_CUTOFF, false));
+    	BestMove<M> bestMove = POOL.invoke(new GetBestMoveTask<M, B>(moves, board, 0, moves.size(), this.evaluator, ply, cutoff, DIVIDE_CUTOFF));
     	return bestMove.move;
     }
 }
