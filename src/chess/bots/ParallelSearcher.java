45c1017cbc5e370d2a7852701c1484b5e2867548
Pichayut Liamthong <pichl@uw.edu>
2017-02-21 15:05:43 -0800

    divideCutoff = 4

--- src/chess/bots/ParallelSearcher.java
+++ src/chess/bots/ParallelSearcher.java
@@ -14,7 +14,7 @@ public class ParallelSearcher<M extends Move<M>, B extends Board<M, B>> extends
         AbstractSearcher<M, B> {
 	
 	private static ForkJoinPool POOL = new ForkJoinPool();
-	private static final int DIVIDE_CUTOFF = 32;
+	private static final int DIVIDE_CUTOFF = 4;
 	
 	@SuppressWarnings("serial")
 	static class GetBestMoveTask<M extends Move<M>, B extends Board<M, B>> extends RecursiveTask<BestMove<M>> {
@@ -55,18 +55,17 @@ public class ParallelSearcher<M extends Move<M>, B extends Board<M, B>> extends
 		    		return SimpleSearcher.minimax(e, board, depth, moves);
 				}
 				List<GetBestMoveTask<M, B>> taskList = new ArrayList<GetBestMoveTask<M, B>>();
-				board.applyMove(moves.get(l));
-				taskList.add(new GetBestMoveTask<M, B>(null, board.copy(), 0, -1, e, depth - 1, cutoff, divideCutoff, true));
-				board.undoMove();
-				for(int i = l + 1; i < r; i++) {
+				for(int i = l; i < r - 1; i++) {
 					board.applyMove(moves.get(i));
 					taskList.add(new GetBestMoveTask<M, B>(null, board.copy(), 0, -1, e, depth - 1, cutoff, divideCutoff, true));
 					taskList.get(i - l).fork();
 					board.undoMove();
 				}
-				int bestValue = taskList.get(0).compute().negate().value;
-				M bestMove = moves.get(l);
-				for(int i = 1; i < taskList.size(); i++) {
+				board.applyMove(moves.get(r - 1));
+				taskList.add(new GetBestMoveTask<M, B>(null, board, 0, -1, e, depth - 1, cutoff, divideCutoff, true));
+				int bestValue = taskList.get(this.size() - 1).compute().negate().value;
+				M bestMove = moves.get(r - 1);
+				for(int i = 0; i < taskList.size() - 1; i++) {
 					int newValue = taskList.get(i).join().negate().value;
 		    		if(newValue > bestValue) {
 		    			bestValue = newValue;
