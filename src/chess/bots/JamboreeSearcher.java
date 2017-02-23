969b2860f1941eefab19a13d57271536764129d9
Pichayut Liamthong <pichl@uw.edu>
2017-02-22 20:54:21 -0800

    Jamboree(2)

--- src/chess/bots/JamboreeSearcher.java
+++ src/chess/bots/JamboreeSearcher.java
@@ -14,11 +14,12 @@ public class JamboreeSearcher<M extends Move<M>, B extends Board<M, B>> extends
         AbstractSearcher<M, B> {
 	
 	private static ForkJoinPool POOL = new ForkJoinPool();
-    private static final double PERCENTAGE_SEQUENTIAL = 0.5;
+    private static final double PERCENTAGE_SEQUENTIAL = 0.67; // 0.4375
+    private static final int DIVIDE_CUTOFF = 1000000;
     
     public M getBestMove(B board, int myTime, int opTime) {
         /* Calculate the best move */
-    	BestMove<M> bestMove = POOL.invoke(new JamboreeSubTask<M, B>(this.evaluator, board, ply, null, -this.evaluator.infty(), this.evaluator.infty(), cutoff));
+    	BestMove<M> bestMove = POOL.invoke(new JamboreeSubTask<M, B>(this.evaluator, board, ply, null, 0, -1, -this.evaluator.infty(), this.evaluator.infty(), cutoff, DIVIDE_CUTOFF, false));
         return bestMove.move;
     }
     
@@ -27,9 +28,11 @@ public class JamboreeSearcher<M extends Move<M>, B extends Board<M, B>> extends
     	B board;
     	Evaluator<B> e;
     	int depth, alpha, beta;
-    	int cutoff;
+    	int cutoff, divideCutoff;
+    	int l, r;
+    	boolean AlreadyHaveGoodAlphaBeta;
     	
-    	public JamboreeSubTask(Evaluator<B> e, B board, int depth, List<M> moves, int alpha, int beta, int cutoff) {
+    	public JamboreeSubTask(Evaluator<B> e, B board, int depth, List<M> moves, int l, int r, int alpha, int beta, int cutoff, int divideCutoff, boolean AlreadyHaveGoodAlphaBeta) {
     		this.e = e;
     		this.board = board;
     		this.depth = depth;
@@ -37,57 +40,107 @@ public class JamboreeSearcher<M extends Move<M>, B extends Board<M, B>> extends
     		this.alpha = alpha;
     		this.beta = beta;
     		this.cutoff = cutoff;
+    		this.l = l;
+    		this.r = r;
+    		this.divideCutoff = divideCutoff;
+    		this.AlreadyHaveGoodAlphaBeta = AlreadyHaveGoodAlphaBeta;
     	}
     	
+    	public int size() {
+			return r - l;
+		}
+    	
 		protected BestMove<M> compute() {
-			if(this.moves == null) {
-	    		this.moves = board.generateMoves();
-	    	}
-			
-			if(this.depth <= this.cutoff || this.moves.size() == 0) {
-				return AlphaBetaSearcher.alphaBeta(this.e, this.board, this.depth, this.moves, this.alpha, this.beta);
+			M bestMove = null;
+			if(!AlreadyHaveGoodAlphaBeta) {
+				this.board = this.board.copy();
+				if(this.moves == null) {
+		    		this.moves = this.board.generateMoves();
+		    		this.r = this.moves.size();
+		    	}
+				
+				if(this.depth <= this.cutoff || this.moves.size() == 0) {
+					return AlphaBetaSearcher.alphaBeta(this.e, this.board, this.depth, this.moves, this.alpha, this.beta);
+				}
+		    	for (int i = l; i < l + (int) (PERCENTAGE_SEQUENTIAL * this.size()); i++) {
+		    		M move = this.moves.get(i);
+		    		this.board.applyMove(move);
+		    		int value = new JamboreeSubTask<M, B>(this.e, this.board, this.depth - 1, null, 0, -1, -this.beta, -this.alpha, this.cutoff, this.divideCutoff, false).compute().negate().value;
+		    		this.board.undoMove();
+		    		if (value > alpha) {
+		    			alpha = value;
+		    			bestMove = move;
+		    		}
+		    		if (alpha >= beta) {
+		    			return new BestMove<M>(bestMove, alpha);
+		    		}
+		    	}
+			}
+
+			int st, ed;
+			if(!AlreadyHaveGoodAlphaBeta) {
+				st = l + (int) (PERCENTAGE_SEQUENTIAL * this.size());
+				ed = r;
+			} else {
+				st = l;
+				ed = r;
 			}
 			
-			M bestMove = null;
-	    	for (int i = 0; i < (int) (PERCENTAGE_SEQUENTIAL * moves.size()); i++) {
-	    		M move = this.moves.get(i);
-	    		this.board.applyMove(move);
-	    		int value = new JamboreeSubTask<M, B>(this.e, this.board, this.depth - 1, null, -this.beta, -this.alpha, this.cutoff).compute().negate().value;
-	    		this.board.undoMove();
-	    		if (value > alpha) {
-	    			alpha = value;
-	    			bestMove = move;
-	    		}
-	    		if (alpha >= beta) {
-	    			return new BestMove<M>(bestMove, alpha);
-	    		}
-	    	}
-	    	
-	    	List<JamboreeSubTask<M, B>> taskList = new ArrayList<JamboreeSubTask<M, B>>();
-	    	for (int i = (int) (PERCENTAGE_SEQUENTIAL * moves.size()); i < moves.size(); i++) {
-	    		board.applyMove(this.moves.get(i));
-	    		taskList.add(new JamboreeSubTask<M, B>(this.e, this.board.copy(), this.depth - 1, null, -this.beta, -this.alpha, this.cutoff));
-	    		if(i < moves.size() - 1) {
-	    			taskList.get(i - (int) (PERCENTAGE_SEQUENTIAL * moves.size())).fork();
-	    		}
-	    		board.undoMove();
-	    	}
-	    	for(int i = taskList.size() - 1; i >= 0; i--) {
-	    		int value;
-	    		if(i == taskList.size() - 1) {
-	    			value = taskList.get(i).compute().negate().value;
-	    		} else {
-	    			value = taskList.get(i).join().negate().value;
-	    		}
-	    		if (value > alpha) {
+			if(this.size() > this.divideCutoff) {
+				JamboreeSubTask<M, B> leftTask = new JamboreeSubTask<M, B>(e, board, depth, moves, st, st + (ed - st) / 2, alpha, beta, cutoff, divideCutoff, true);
+				JamboreeSubTask<M, B> rightTask = new JamboreeSubTask<M, B>(e, board, depth, moves, st + (ed - st) / 2, ed, alpha, beta, cutoff, divideCutoff, true);
+				
+				leftTask.fork();
+				BestMove<M> answer = rightTask.compute();
+				if(answer.value > alpha) {
+					alpha = answer.value;
+					bestMove = answer.move;
+				}
+				BestMove<M> leftAnswer = leftTask.join();
+				if(leftAnswer.value > alpha) {
+					alpha = leftAnswer.value;
+					bestMove = leftAnswer.move;
+				}
+				return new BestMove<M>(bestMove, alpha);
+			} else {		
+				this.board = this.board.copy();
+				if(this.moves == null) {
+		    		this.moves = this.board.generateMoves();
+		    		this.r = this.moves.size();
+		    	}
+				List<JamboreeSubTask<M, B>> taskList = new ArrayList<JamboreeSubTask<M, B>>();
+		    	for (int i = st; i < ed - 1; i++) {
+		    		board.applyMove(this.moves.get(i));
+		    		taskList.add(new JamboreeSubTask<M, B>(this.e, this.board.copy(), this.depth - 1, null, 0, -1, -this.beta, -this.alpha, this.cutoff, this.divideCutoff, false));
+		    		taskList.get(i - st).fork();
+		    		board.undoMove();
+		    	}
+		    	
+		    	// do one work yourself
+		    	board.applyMove(this.moves.get(ed - 1));
+		    	JamboreeSubTask<M, B> current = new JamboreeSubTask<M, B>(this.e, this.board, this.depth - 1, null, 0, -1, -this.beta, -this.alpha, this.cutoff, this.divideCutoff, false);
+		    	int value = current.compute().negate().value;
+		    	if (value > alpha) {
 		    		alpha = value;
-		    		bestMove = moves.get(i + (int) (PERCENTAGE_SEQUENTIAL * moves.size()));
+		    		bestMove = this.moves.get(ed - 1);
 		    	}
 		    	if (alpha >= beta) {
 		    		return new BestMove<M>(bestMove, alpha);
 		    	}
-	    	}
-	    	return new BestMove<M>(bestMove, alpha);
+		    	//---------------------
+		    	
+		    	for(int i = 0; i < taskList.size(); i++) {
+		    		value = taskList.get(i).join().negate().value;
+		    		if (value > alpha) {
+			    		alpha = value;
+			    		bestMove = moves.get(i + st);
+			    	}
+			    	if (alpha >= beta) {
+			    		return new BestMove<M>(bestMove, alpha);
+			    	}
+		    	}
+		    	return new BestMove<M>(bestMove, alpha);
+			}
 		}
     }
 }
