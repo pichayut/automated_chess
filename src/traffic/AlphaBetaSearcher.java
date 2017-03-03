d8ba0b50a77bda5f0345218c7baeb3dc073f5126
Pichayut Liamthong <pichl@uw.edu>
2017-03-03 15:54:08 -0800

    fix

--- src/traffic/AlphaBetaSearcher.java
+++ src/traffic/AlphaBetaSearcher.java
@@ -29,7 +29,7 @@ public class AlphaBetaSearcher<M extends Move<M>, B extends Board<M, B>> extends
     		} else {
     			return new BestMove<M>(-evaluator.stalemate());
     		}
-    	}
+    	} 
     	
     	M bestMove = null;
     	for(M move : moves) {
