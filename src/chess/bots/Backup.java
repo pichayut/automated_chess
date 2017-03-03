d5b97634e22d67a24baa5b27c943dce888f437a7
jacob <jacob@LAPTOP-1AQ0G6JB>
2017-03-03 15:30:57 -0800

    merge

--- src/chess/bots/Backup.java
+++ src/chess/bots/Backup.java
@@ -18,7 +18,7 @@ public class Backup<M extends Move<M>, B extends Board<M, B>> extends
 	        	
 	private static ForkJoinPool POOL = new ForkJoinPool();
     private static final double PERCENTAGE_SEQUENTIAL = 0.5; //0.4375;
-    private static final int DIVIDE_CUTOFF = 2;
+    private static final int DIVIDE_CUTOFF = 2; 
         
     public M getBestMove(B board, int myTime, int opTime) {
         /* Calculate the best move */
