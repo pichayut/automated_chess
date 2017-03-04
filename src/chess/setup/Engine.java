4e64bd7af6786fccf522a0c1b73c4aff88afdcc7
Pichayut Liamthong <pichl@uw.edu>
2017-03-04 12:26:23 -0800

    cutoff = 2

--- src/chess/setup/Engine.java
+++ src/chess/setup/Engine.java
@@ -25,7 +25,7 @@ public class Engine {
 	
 	public Engine(int time, int inc) {
         searcher.setDepth(7);
-        searcher.setCutoff(0);
+        searcher.setCutoff(2);
 
         searcher.setEvaluator(new SimpleEvaluator());
         searcher.setTimer(new SimpleTimer(time, inc));
