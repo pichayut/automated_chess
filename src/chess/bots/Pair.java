af3bc253aa8d78fdd5be04c00d2ac018cd18443b
Pichayut Liamthong <pichl@uw.edu>
2017-03-09 23:32:10 -0800

    fix staticAnalysis

--- src/chess/bots/Pair.java
+++ src/chess/bots/Pair.java
@@ -25,4 +25,9 @@ public class Pair {
 		Pair o = (Pair) obj;
 		return this.bestMove.move.equals(o.bestMove.move) && this.bestMove.value == o.bestMove.value && this.depth == o.depth;
 	}
+	
+	@Override
+	public int hashCode() {
+		return bestMove.hashCode() * 31 + depth;
+	}
 }
